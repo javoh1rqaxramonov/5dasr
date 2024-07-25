ba9e17d6782330174245abe21ea866d006db2653
javoh1rqaxramonov <ubotirov22@.com>
2024-07-25 12:58:02 +0500

    finished

---
+++ index.js
@@ -0,0 +1,80 @@
+// 1-masala
+// const a = [1, 2, 3, 4, 5];
+// const b = a.map(num => num * 2);
+// console.log(b); 
+
+// 2-masala
+// const a=[1,2,3,4];
+// const b=a.reverse ();
+// console.log(b);
+
+// 3-masala 
+// const a= [1,2,3,4,5,-6];
+// const b = a.filter(num => num >= 0);
+// console.log(b); 
+
+// 4-masala 
+// const a = [1, 2, 3, 4, 5];
+// const sum = a.reduce((accumulator, current) => accumulator + current);
+// console.log(sum); 
+
+// 5-masala 
+// const a = [1, 2, 3, 4, 5];
+// const b = 6;
+// a.push(b);
+// console.log(a); 
+
+// 6-masala 
+// const a = [1, 2, 3, 4, 5];
+// const b = a.pop();
+// console.log(a); 
+
+// 7-masala 
+// const a = [1, 2, 3, 4, 5];
+// const b = a.shift();
+// console.log(a); 
+
+// 8-masala 
+// const a = [1, 2, 3, 4, 5];
+// const b = 6;
+// a.unshift(b);
+// console.log(a);
+
+// 10-masala 
+// const a = [1, 2, 3, 4, 5];
+// const b = a.slice();
+// console.log(b); 
+
+// 11-masala 
+// const a = [5, 12, 8, 130, 44];
+// const maxb = Math.max(...a);
+// console.log(maxb); 
+
+// 12-masala 
+// const a = [5, 12, 8, 130, 44];
+// const minb = Math.min(...a);
+// console.log(minb); 
+
+// 13-masala
+// const a = [5, 12, 8, 130, 44, 3];
+// const b = a.filter(num => num % 2 === 0).length;
+// console.log(b); 
+
+// 14-masala 
+// const a = [5, 12, 8, 130, 44, 3];
+// const b = a.filter(num => num % 2 === 1).length;
+// console.log(b); 
+
+// 15-masala 
+// const a = [0, 5, 12, 0, 8, 130, 44, 0, 3, 7, 10, 0];
+// const b = a.filter(num => num !== 0);
+// console.log(b); 
+
+// 16-masala 
+const a = [1, 2, 3, 4, 5];
+const b = a.flatMap(num => [num, num]);
+console.log(b); 
+
+
+
+
